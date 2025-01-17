cb207f1bab777edb0d8fff789754fd69918f142a
markunca <martin.kuncicky@gmail.com>
2025-01-17 14:32:08 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+  "name": "Housee HA Addon",
+  "version": "1.0.0",
+  "slug": "housee-ha-addony",
+  "description": "Conrolling HA by Housee",
+  "arch": ["armv7", "arm64", "amd64", "i386"],
+  "startup": "application",
+  "boot": "auto",
+  "ingress": false,
+  "options": {},
+  "schema": {},
+  "ports": {
+    "5000/tcp": 5000
+  },
+  "host_network": true
+}
